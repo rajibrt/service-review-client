0edea8dabc00488e79e0531d0541483ceb0433ac
rajibrt <rajibrt@gmail.com>
2022-11-08 06:51:51 +0600

    Here text size adjsuted

--- src/Pages/Home/Hero/HeroItem.js
+++ src/Pages/Home/Hero/HeroItem.js
@@ -17,9 +17,9 @@ const HeroItem = ({ slide }) => {
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href={`#slide${prev}`} className="bg-transparent border-none md:text-3xl font-bold text-white hover:text-yellow-500 hover:bg-transparent">PREV</a>
                 <div>
-                    <h2 data-aos="zoom-out-down" className="md:text-6xl font-bold text-white">on <span className="text-yellow-500">Click</span> </h2>
-                    <h1 data-aos="flip-left" className="md:text-4xl font-bold text-white md:my-6">The moment for <span className=" text-yellow-500">a few seconds</span> <br />but I capture it for a <span className="text-yellow-500">lifetime</span></h1>
-                    <p className="text-white">"It's not enough to just own a camera.<br /> Everyone owns a camera. <br />To be a photographer, you must understand, appreciate,<br /> and harness the power you hold!"</p>
+                    <h2 data-aos="zoom-out-down" className="md:text-6xl text-2xl font-bold text-white">on <span className="text-yellow-500">Click</span> </h2>
+                    <h1 data-aos="flip-left" className="md:text-4xl text-base font-bold text-white md:my-6">The moment for <span className=" text-yellow-500">a few seconds</span> <br />but I capture it for a <span className="text-yellow-500">lifetime</span></h1>
+                    <p className="text-white md:text-base text-sm">"It's not enough to just own a camera.<br /> Everyone owns a camera. <br />To be a photographer, you must understand, appreciate,<br /> and harness the power you hold!"</p>
                 </div>
                 <a href={`#slide${next}`} className="bg-transparent border-none md:text-3xl font-bold text-white hover:text-yellow-500 hover:bg-transparent ">NEXT</a>
             </div>
